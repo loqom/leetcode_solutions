cb8fc6c9793d1ad20c8a05ace9831bd3bc9e97e7
loqom <omgkplko@gmail.com>
2025-08-05 18:10:25 +0000

    Code challenge submission for question: 1402 Reducing Dishes

---
+++ 1402 Reducing Dishes/1402reducing-dishes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        if (satisfaction.empty()) return 0;
+        sort(satisfaction.begin(),satisfaction.end());
+        int n=satisfaction.size();
+        vector<int>suf(n);
+        suf[n-1]=satisfaction[n-1];
+        for(int i=n-2;i>=0;i--){
+            suf[i]=suf[i+1]+satisfaction[i];
+        }
+        int idx=-1;
+        for(int i=0;i<n;i++){
+            if(suf[i]>0){
+                idx=i;
+                break;
+            } 
+        }
+        if(idx==-1) return 0;
+        int cn=0;
+        int p=1;
+        for(int i=idx;i<n;i++){
+            cn+=p*satisfaction[i];
+            p++;
+        }
+        return cn;
+    }
+};
