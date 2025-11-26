1f502c453b1f8c9544c8c5fa3aa893e889a485c1
loqom <omgkplko@gmail.com>
2025-11-26 06:17:54 +0000

    Code challenge submission for question: 66 Plus One

---
+++ 66 Plus One/66plus-one.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]==9) digits[i]=0;
+            else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        vector<int> res(n+1);
+        res[0]=1;
+        return res;
+    }
+};
