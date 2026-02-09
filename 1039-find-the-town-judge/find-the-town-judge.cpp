a9bdb5cddaeec380cab2ba90315a2cafa713630e
Om Vishwakarma <omgkplko@gmail.com>
2026-02-09 19:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 64.8 MB (99.45%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        for(int i=1;i<=n;i++){
+            int p=0,x=0;
+            for(vector<int>& t:trust){
+                if(t[0]==i){
+                    p++;
+                    break;
+                }
+                else if(t[1]==i) x++;
+            }
+            if(p==0 && x ==n-1) return i;
+        }
+        return -1;
+    }
+};
