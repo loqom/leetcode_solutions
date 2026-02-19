4a8d14c53699bc10b22504c303c3c3dd702e008d
Om Vishwakarma <omgkplko@gmail.com>
2026-02-20 01:41:24 +0530

    Time: 15 ms (88.87%) | Memory: 139.3 MB (43.48%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if (k==1) return nums;
+        int n=nums.size();
+        vector<int> res;
+        deque<int>dq;
+        for(int i=0;i<n;i++){
+            while(dq.size()>0 && nums[dq.back()]<nums[i]) dq.pop_back();
+            dq.push_back(i);
+            int j=i-k+1;
+            while(dq.front()<j) dq.pop_front();
+            if(i>=k-1) res.push_back(nums[dq.front()]);
+        }
+        return res;
+
+    }
+};
