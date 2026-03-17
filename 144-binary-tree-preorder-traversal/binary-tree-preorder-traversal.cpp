2b27264da82ac09df99766a71f8a98bab2acf71b
Om Vishwakarma <omgkplko@gmail.com>
2026-03-17 12:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (9.26%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,28 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-
-    void traverse(TreeNode* root,vector<int> &res){
-        if(root==NULL) return;
-        res.push_back(root->val);
-        traverse(root->left,res);
-        traverse(root->right,res);
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-        
+    // void traverse(TreeNode* root,vector<int> &res){
+    //     if(root==NULL) return;
+    //     res.push_back(root->val);
+    //     traverse(root->left,res);
+    //     traverse(root->right,res);
+    // }
+    vector<int> preorderTraversal(TreeNode* root) {   
+        if(root==NULL) return {};    
         vector<int>res;
-        traverse(root,res);
-        return res;
+        stack<TreeNode *> st;
+        st.push(root);
+        while(st.size()>0){
+            TreeNode* temp=st.top();
+            st.pop();
+            res.push_back(temp->val);
+            if(temp->right!=NULL) st.push(temp->right);
+            if(temp->left!=NULL) st.push(temp->left); 
+        }
+        return res; 
 
     }
 };
