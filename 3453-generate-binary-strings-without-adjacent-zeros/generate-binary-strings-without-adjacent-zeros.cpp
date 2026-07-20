ac6281e12f305efb4888af0b78db5dbd211d5b7d
Om Vishwakarma <omgkplko@gmail.com>
2026-07-20 15:59:42 +0530

    Time: 12 ms (69.29%) | Memory: 29.6 MB (29.47%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    void helper(vector<string> &ans,string s,int i,int n){
+    void helper(vector<string> &ans,string s,int i,int n,vector<string> &dp){
         if(i==n){
             ans.push_back(s);
+            dp[i]=s;
             return;
         }
-        if(s.size()==0 || (s.size()>=1 && s[s.size()-1]!='0')) helper(ans,s+"0",i+1,n);
-        helper(ans,s+"1",i+1,n);
+        if(dp[i]!="") {
+            ans.push_back(dp[i]);
+            return;
+        }
+        if(s.size()==0 || (s.size()>=1 && s[s.size()-1]!='0')) helper(ans,s+"0",i+1,n,dp);
+        helper(ans,s+"1",i+1,n,dp);
     }
     vector<string> validStrings(int n) {
+        vector<string> dp(n+1,"");
         vector<string> ans;
         string s="";
-        helper(ans,s,0,n);
+        helper(ans,s,0,n,dp);
         return ans;
     }
 };
