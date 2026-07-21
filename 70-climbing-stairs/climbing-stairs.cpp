76739a77f0f99e17c1fd05146ff5dff3ee9354d7
Om Vishwakarma <omgkplko@gmail.com>
2026-07-21 23:50:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.03%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int>dp;
     int count(int n){
-        if(n==2 || n==1|| n<=0) return n;
+        if(n<=2) return n;
         if(dp[n]!=-1) return dp[n];
         return dp[n]=count(n-1)+count(n-2);
     }
