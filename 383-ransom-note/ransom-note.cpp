1fd315fa3ae66740b1f110c6b3360760984f1c9d
Om Vishwakarma <omgkplko@gmail.com>
2026-08-14 09:31:27 +0530

    Time: 17 ms (5.19%) | Memory: 11.6 MB (89.87%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int cn=0;
+        int i=0;
+        int j=0;
+        int n=ransomNote.size();
+        int m=magazine.size();
+        sort(magazine.begin(),magazine.end());
+        sort(ransomNote.begin(),ransomNote.end());
+        while (i < n && j < m) {
+            if (ransomNote[i] == magazine[j]) {
+                i++;
+                j++;
+            }
+            else if (ransomNote[i] > magazine[j]) j++;
+            else return false;
+        }
+        return i == n;
+    }
+};
